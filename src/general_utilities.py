06159fa1076171fc458178c60de8e3543bdcee20
gastonholmen <gastonholmen@hotmail.com>
2020-01-22 21:45:18 +0100

    Create directory if it does not exist in save_parquet

--- src/general_utilities.py
+++ src/general_utilities.py
@@ -4,6 +4,9 @@ import os
 
 def save_parquet(df: pd.DataFrame, path: str, overwrite=True, save_excel=True):
     path = os.path.abspath(path)
+    path_dir = os.path.split(path)[0]
+    if not os.path.isdir(path_dir):
+        os.makedirs(path_dir)
     if not os.path.isfile(path) or overwrite:
         for c in df.columns:
             df = df.assign(**{c: df[c].astype(str)})
